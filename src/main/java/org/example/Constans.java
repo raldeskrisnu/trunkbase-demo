74d1d57df384af1f187027d2e32e47a62cd333bf
raldeskrisnu <raldes.pratama@happyfresh.com>
2024-05-30 21:14:02 +0700

    [RFC-3] Add banner test

--- src/main/java/org/example/Constans.java
+++ src/main/java/org/example/Constans.java
@@ -5,4 +5,5 @@ public class Constans {
     public static int LIFE_TIME = 258;
     public static int TIME_OUT = 258;
     public static String BANNER_NAME = "lorem_ipsum.jpg";
+    public static String BANNER_TEST = "test.jpg";
 }
